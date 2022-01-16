a98085ce4fe18650116ce9810531a675f92463b8
AdityaAshish <81067181+AdityaAshish@users.noreply.github.com>
2022-01-16 13:16:46 +0530

    Add files via upload

--- LetsChatLogin.js
+++ LetsChatLogin.js
@@ -1,21 +1,5 @@
-
-//ADD YOUR FIREBASE LINKS HERE
-var firebaseConfig = {
-      apiKey: "AIzaSyD9mQnBm4O40C_CIXF89rBXVFceFRFi7Lw",
-      authDomain: "kwitter-bc148.firebaseapp.com",
-      databaseURL: "https://kwitter-bc148-default-rtdb.firebaseio.com",
-      projectId: "kwitter-bc148",
-      storageBucket: "kwitter-bc148.appspot.com",
-      messagingSenderId: "200000766482",
-      appId: "1:200000766482:web:5f43d11ae57771eec5cecb"
-    };
-    
-    // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-
-      //End code
-      });});}
-getData();
+function Login() {
+    user_name= document.getElementById("Username_inputbox").value;
+    localStorage.setItem("user name", user_name);
+    window.location="LetsChat.html";
+}
